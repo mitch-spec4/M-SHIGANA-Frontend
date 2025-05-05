bac93aa0568e4faddcfb5b7b2948ca66e4b64b53
Elisha Karanja <elishadominic74@gmail.com>
2025-05-05 16:36:48 +0300

    initialize state

--- AdminDashboard.jsx
+++ AdminDashboard.jsx
@@ -1,2 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+
+function AdminDashboard() {
+    const [users, setUsers] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [error, setError] = useState(null)
+  
+}
