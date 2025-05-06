a175cce2bbdf1c1143aea6f06875a68670590c2a
Steamgx <your-bmuraya646@gmail.com>
2025-05-06 07:07:25 +0300

    DEBUG

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -147,7 +147,7 @@ function App() {
       <div
         className="container"
         style={{
-          backgroundImage: 'url("https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fG1vbmV5JTIwdHJhbnNmZXJ8ZW58MHx8fHwxNjg3NTYyNzQ0&ixlib=rb-4.0.3&q=80&w=1080")',
+          backgroundImage: 'url("https://cdn.pixabay.com/animation/2022/09/09/21/19/21-19-47-383_512.gif")',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat',
