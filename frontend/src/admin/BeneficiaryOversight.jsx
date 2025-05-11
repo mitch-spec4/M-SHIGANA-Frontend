bea024f6af3f7177fccb49a2f4a75e41dda4c103
eve <evewangui447@gmail.com>
2025-05-11 19:14:27 +0300

    beneficiary oversight

--- frontend/src/admin/BeneficiaryOversight.jsx
+++ frontend/src/admin/BeneficiaryOversight.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function BeneficiaryOversight() {
+  const [beneficiaries, setBeneficiaries] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/beneficiaries")
+      .then((res) => setBeneficiaries(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div>
-      <h2>Beneficiary Oversight</h2>
-      <p>Placeholder for Beneficiary Oversight feature.</p>
+      <h2>Beneficiaries</h2>
+      <ul>
+        {beneficiaries.map((b) => (
+          <li key={b.id}>{b.name}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default BeneficiaryOversight
+export default BeneficiaryOversight;
